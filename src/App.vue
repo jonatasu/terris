77719d8ffab92ce94246899359b3e54a30cff312
Jônatas F. Vieira <jonatas.vieira@contabilizei.com.br>
2022-09-03 17:07:55 -0300

    feat(App.vue): Update navigation drawer content

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,40 @@
       v-model="drawer"
       app
     >
-      <!--  -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Terris
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Terris</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
@@ -21,6 +48,13 @@
 
 <script>
 export default {
-  data: () => ({ drawer: null }),
+  data: () => ({
+    items: [
+      { title: 'Home', icon: 'mdi-map-search' },
+      // { title: 'Photos', icon: 'mdi-image' },
+      // { title: 'About', icon: 'mdi-help-box' },
+    ],
+    drawer: null,
+  }),
 };
 </script>
