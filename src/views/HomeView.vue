9b49a13fb621a1ea687fcdab65129e44c21faa8a
Jônatas F. Vieira <jonatas.vieira@contabilizei.com.br>
2022-09-05 02:31:19 -0300

    style(HomeView): Adjusted the logo in the middle of the screen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,27 @@
 <template>
   <v-container class="fill-height align-items-center justify-center">
-    <v-flex>
-      <v-flex class="overflow-hidden rounded-xl mb-5">
-        <v-img
-          :src="require($vuetify.theme.dark
-          ? '../assets/logo/DarkMode-V2-cropped.jpg'
-          : '../assets/logo/LightMode-V2-cropped.jpg')"
-          alt="TERRIS"
-        />
-      </v-flex>
-      <v-spacer class="pt-5">
-        <SearchCountryInput />
-      </v-spacer>
-    </v-flex>
+    <v-row class="justify-center">
+      <v-col
+        cols="10"
+        sm="7"
+        lg="3"
+        class="d-flex flex-column align-items-center"
+      >
+        <v-flex class="d-flex overflow-hidden rounded-xl mb-5">
+          <v-img
+            :src="require($vuetify.theme.dark
+            ? '../assets/logo/DarkMode-V2-cropped.jpg'
+            : '../assets/logo/LightMode-V2-cropped.jpg')"
+            alt="TERRIS"
+            max-width="500"
+            width="100%"
+          />
+        </v-flex>
+        <v-spacer class="pt-5">
+          <SearchCountryInput />
+        </v-spacer>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
