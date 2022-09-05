3b40baa711645161c8c06858173a43f7ce8fa079
Jônatas F. Vieira <jonatas.vieira@contabilizei.com.br>
2022-09-05 02:05:26 -0300

    feat(CountryView-CountryInfoCard): Developed the details page of the selected country
    Added some icons to represent some informations. Also, created a card component.

---
+++ src/components/CountryInfoCard.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-card
+    class="d-flex pa-3 mb-5 flex-row align-items-center"
+    elevation="0"
+    outlined
+    shaped
+  >
+    <v-card
+      class="country-info-icon-frame overflow-hidden mr-3"
+      width="50"
+      height="50"
+      outlined
+      shaped
+      elevation="0"
+    >
+      <v-img
+        v-if="iconName"
+        class="align-self-start"
+        :src="require(`../assets/icons/${iconName}.svg`)"
+        max-width="50"
+        max-height="50"
+      />
+    </v-card>
+    <ul
+      v-if="infoContentType === 'list' && infoAmount > 1"
+      class="d-flex flex-column justify-center pl-3"
+    >
+      <li
+        v-for="(content, index) in infoContent"
+        :key="`content_${index}`"
+      >
+        <p class="font-weight-medium pa-0 ma-0">{{ content }}</p>
+      </li>
+    </ul>
+    <v-flex
+      v-if="infoAmount === 1"
+      class="d-flex align-center"
+    >
+      <p class="font-weight-medium pa-0 ma-0">
+        {{ infoContentType === 'list' ? infoContent[0] : infoContent }}
+      </p>
+    </v-flex>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    iconName: {
+      type: String,
+    },
+    infoContentType: {
+      type: String,
+      default: 'text',
+    },
+    infoContent: {
+      type: [String, Array],
+      required: true,
+    },
+  },
+  computed: {
+    infoAmount() {
+      let amount = 1;
+      if (this.infoContentType === 'list' && this.infoContent.length > 1) {
+        amount = this.infoContent.length;
+      }
+      return amount;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.theme--dark.v-card.country-info-icon-frame {
+  background-color: #F8F9FB;
+}
+</style>
--- src/views/CountryView.vue
+++ src/views/CountryView.vue
@@ -1,16 +1,94 @@
 <template>
   <v-container v-if="country" class="country">
-    <h1>{{ country.name.official }}</h1>
-    <p>{{ country }}</p>
+    <v-row class="pt-5">
+      <v-col cols="8">
+        <h2 class="mb-3">{{ country.name.official }} ({{ country.cca2 }})</h2>
+        <v-row class="mb-3">
+          <v-col cols="6">
+            <CountryInfoCard
+              icon-name="continent"
+              info-content-type="list"
+              :info-content="getContinent"
+            />
+            <CountryInfoCard
+              icon-name="currency"
+              info-content-type="list"
+              :info-content="getCurrencies"
+            />
+            <CountryInfoCard
+              icon-name="population"
+              :info-content="getPopulation"
+            />
+          </v-col>
+          <v-col cols="6">
+            <CountryInfoCard
+              icon-name="capital"
+              info-content-type="list"
+              :info-content="country.capital"
+            />
+            <CountryInfoCard
+              icon-name="language"
+              info-content-type="list"
+              :info-content="getLanguages"
+            />
+            <CountryInfoCard
+              icon-name="area"
+              :info-content="getArea"
+            />
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="4">
+        <v-btn
+          color="primary"
+          class="mb-3"
+          block
+          elevation="0"
+          text
+          @click="gotoGoogleMaps()"
+        >
+          <v-icon left large dense class="mr-3">
+            mdi-google-maps
+          </v-icon>
+          Open on Google Maps
+          <v-icon class="ml-3" x-small right>
+            mdi-open-in-new
+          </v-icon>
+        </v-btn>
+        <v-card class="mb-5">
+          <v-img
+            :src="country.flags.svg"
+            :alt="`flag of ${country.name.official}`"
+          />
+        </v-card>
+      </v-col>
+    </v-row>
+    <blockquote
+      v-if="country.unMember"
+      class="d-flex align-center justify-center px-sm-5 mb-5"
+    >
+      <v-img
+        :src="require('@/assets/icons/united-nations.png')"
+        max-width="50"
+        max-height="50"
+      />
+      <p class="title font-weight-medium mb-0 ml-3">
+        This country is a member of the United Nations
+      </p>
+    </blockquote>
   </v-container>
 </template>
 
 <script>
 import * as Api from '@/service';
 import { mapState, mapActions } from 'vuex';
+import CountryInfoCard from '@/components/CountryInfoCard.vue';
 
 export default {
   name: 'CountryView',
+  components: {
+    CountryInfoCard,
+  },
   computed: {
     ...mapState(['visitedCountries']),
     countryCode() {
@@ -21,6 +99,24 @@ export default {
     },
     hasStoredCountry() {
       return !!this.visitedCountries[this.countryCode];
+    },
+    getCurrencies() {
+      const currenciesList = Object.values(this.country.currencies);
+      return currenciesList.map(
+        (item) => `${item.symbol} - ${item.name}`,
+      );
+    },
+    getContinent() {
+      return Object.values(this.country.continents);
+    },
+    getLanguages() {
+      return Object.values(this.country.languages);
+    },
+    getPopulation() {
+      return `Population of ${Number(this.country.population).toLocaleString()}`;
+    },
+    getArea() {
+      return `${Number(this.country.area).toLocaleString()} m²`;
     },
   },
   data: () => ({
@@ -54,6 +150,11 @@ export default {
         flagImg: this.country.flags.svg,
       };
       this.setAppBarCountryDetails(appBarFlagCountryDetails);
+    },
+
+    gotoGoogleMaps() {
+      const { googleMaps } = this.country.maps;
+      window.open(googleMaps);
     },
   },
 };
